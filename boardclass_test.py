4622bd7cb3584e7b0514509ec4c0b808b37e5429
pawel <pawel850m@gmail.com>
2020-06-14 21:31:05 +0200

    modify setUp

--- boardclass_test.py
+++ boardclass_test.py
@@ -25,7 +25,7 @@ TIE_FIELDS = [(0, 1, 3), (0, 1, 4), (0, 1, 5), (0, 1, 6), (0, 1, 7), (0, 1, 8),
 class BoardTest(unittest.TestCase):
 
     def setUp(self):
-        self.test_board = boardclass.Board()
+        self.test_board = boardclass.Board("O", 0)
 
     def test_win(self):
         for j in range(8):
